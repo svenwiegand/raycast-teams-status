7d4e5ee5caf6fa8d73f3cb580e2e771f0a3bb8b4
Sven Wiegand <sven.wiegand@orgavision.com>
2022-01-02 20:32:29 +0100

    First working version with shortcuts as backend.

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,37 @@
-import { ActionPanel, CopyToClipboardAction, Icon, List } from "@raycast/api";
+import {ActionPanel, ColorLike, Icon, List, OpenInBrowserAction} from "@raycast/api";
+
+const stat = (key: string, title: string, icon: string, color?: ColorLike) => ({
+    id: key,
+    icon: { source: icon, tintColor: color },
+    title,
+    key,
+})
+const status = [
+    stat("available", "Available", Icon.Checkmark, "green"),
+    stat("busy", "Busy", Icon.Desktop, "red"),
+    stat("doNotDisturb", "Do not disturb", Icon.Bubble, "red"),
+    stat("pause", "Pause", Icon.Clock, "orange"),
+    stat("endOfWork", "End of Work", Icon.XmarkCircle, "purple"),
+    stat("weekend", "Weekend", Icon.XmarkCircle, "purple"),
+]
 
 export default function Command() {
-  const items = Array.from(Array(10).keys()).map((key) => {
-    return {
-      id: key,
-      title: "Title " + key,
-      subtitle: "Subtitle",
-      accessoryTitle: "Accessory Title",
-    };
-  });
-
-  return (
-    <List isLoading={false} searchBarPlaceholder="Filter by title...">
-      {items.map((item) => (
-        <List.Item
-          key={item.id}
-          icon="list-icon.png"
-          title={item.title}
-          subtitle={item.subtitle}
-          accessoryTitle={item.accessoryTitle}
-          accessoryIcon={{ source: Icon.Circle }}
-          actions={
-            <ActionPanel>
-              <CopyToClipboardAction content={item.title} />
-            </ActionPanel>
-          }
-        />
-      ))}
-    </List>
-  );
+    return (
+        <List isLoading={false} searchBarPlaceholder="Filter by title...">
+            {status.map(status => (
+                <List.Item
+                    {...status}
+                    actions={
+                        <ActionPanel>
+                            <OpenInBrowserAction
+                                title="Set Status"
+                                icon={Icon.ArrowRight}
+                                url={`shortcuts://run-shortcut?name=Teams%20Status&input=text&text=${status.key}`}
+                            />
+                        </ActionPanel>
+                    }
+                />
+            ))}
+        </List>
+    );
 }
